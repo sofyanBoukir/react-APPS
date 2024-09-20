0a5317a63aa84ce0194862aa359f01589118c77f
Soufian dev <soufianeboukir0@gmail.com>
2024-09-20 18:40:35 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 
 
 function App() {
+
+  const handleClick = () =>{
+    alert("cc soufian");
+  }
+
+
   return (
     <div className="container">
       <center>
         <h1>Soufian </h1>
-        <h3>Test git project</h3>
-        <h3>Test git project</h3>
-        <h3>Test git project</h3>
-        <h3>Test git project</h3>
         <h1>Walid</h1>
-        <h1>Ali</h1>        
+        <h1>Ali</h1>    
+        <p>three students on digital developement at tiznit ISTA</p>
+        <button onClick={handleClick}>Click</button>
       </center>
     </div>
   );
