c005cbd2fa33434277610696b2ee79e7398f5fb0
walidaitbaha <walidbaha17@gmail.com>
2024-09-20 19:53:16 +0100

    ra bddlt chi haja

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
     alert("cc soufian");
   }
 
+  const handleCheck = () =>{
+    if(true){
+      console.log("wa ta sir t9awed");
+      
+    }
+  }
+
 
   return (
     <div className="container">
@@ -15,7 +22,9 @@ function App() {
         <h1>Ali</h1>  
         <h1>react js front end framwork</h1>  
         <p>three students on digital developement at tiznit ISTA</p>
+        
         <button onClick={handleClick}>Click</button>
+        <button onClick={handleCheck}>hh</button>
       </center>
     </div>
   );
