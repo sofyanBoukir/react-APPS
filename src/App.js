acca6fe7907a0be4400f5cfe135be56d20ca1e7d
Soufian dev <soufianeboukir0@gmail.com>
2024-09-20 19:36:20 +0100

    I made changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
       <center>
         <h1>Soufian </h1>
         <h1>Walid</h1>
-        <h1>Ali</h1>    
+        <h1>Ali</h1>  
+        <h1>react js front end framwork</h1>  
         <p>three students on digital developement at tiznit ISTA</p>
         <button onClick={handleClick}>Click</button>
       </center>
