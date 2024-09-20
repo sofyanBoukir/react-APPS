cc2131d22c3a66f585db1fce8211351e25a66286
Soufian dev <soufianeboukir0@gmail.com>
2024-09-20 18:25:33 +0100

    changed the title and somthing other

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
       <center>
         <h1>Soufian </h1>
         <h3>Test git project</h3>
+        <h3>Test git project</h3>
+        <h3>Test git project</h3>
+        <h3>Test git project</h3>
+        <h1>Walid</h1>
+        <h1>Ali</h1>        
       </center>
     </div>
   );
