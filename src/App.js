89599f0b2b3cbe478ac5ac4c874e99e49e5ea0e8
Soufian dev <soufianeboukir0@gmail.com>
2024-09-20 09:46:15 +0100

    Initial commit for React app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <center>
+        <h1>Soufian </h1>
+        <h3>Test git project</h3>
+      </center>
     </div>
   );
 }
