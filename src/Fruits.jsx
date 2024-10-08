d21a033ed2c8197b3e09237a1d3c92fc91c230f4
Soufian dev <soufianeboukir0@gmail.com>
2024-10-08 13:37:07 +0100

    fruits

---
+++ src/Fruits.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import "./styles.css"
+
+export default class Fruits extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {fruits:["soso","koko","dodo"],inputValue:""};
+    }
+
+    handleChange(event){
+        this.setState({inputValue:event.target.value})
+    }
+
+    handleAdd(){
+        this.setState((prevState) => ({
+            fruits: [...prevState.fruits, prevState.inputValue],
+            inputValue: '' // Clear input after adding
+        }));
+    }
+
+    handleDelete(index){
+        let copy = [...this.state.fruits];
+        let deletedItem = copy.filter((item,i) => i !== index);
+        this.setState({fruits:deletedItem});
+    }
+    
+  render() {
+    return (
+      <div className='fruits'>
+        <h1>Fruits lists</h1>
+        <ul>
+            {
+                this.state.fruits.map((fruit,index) =>{
+                    return <li key={index}>{fruit} <button onClick={(index) => this.handleDelete(index)}>X</button></li>
+                })
+            }
+        </ul>
+        <input type='text' 
+        placeholder='Enter somthing to add...' 
+        value={this.state.inputValue} 
+        onChange={(event) => this.handleChange(event)} />
+        <button onClick={() => this.handleAdd()}>Add</button>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+import Fruits from './Fruits';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Fruits />
   </React.StrictMode>
 );
 
