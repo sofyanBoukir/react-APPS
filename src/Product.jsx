c82a19e3f2291761add31dd8318b80bb47d8947a
Soufian dev <soufianeboukir0@gmail.com>
2024-10-09 18:28:36 +0100

    fake store api

---
+++ src/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div className='product'>
+        <img src='' />
+        <h3>Title of product goes here</h3>
+        <span>"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</span>
+    
+    </div>
+  )
+}
+
+export default Product
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import Fruits from './Fruits';
+import Header from './Header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Fruits />
+      <Header />
   </React.StrictMode>
 );
 
